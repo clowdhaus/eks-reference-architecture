fd5e80b1be95bfcb9bd2bae1d5ea42a3717db331
Bryant Biggs <bryantbiggs@gmail.com>
2023-07-21 20:28:36 -0400

    feat: Add ephemeral volume test

---
+++ ephemeral-vol-test/eks.tf
@@ -0,0 +1,59 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.15"
+
+  # create = false
+
+  cluster_name    = local.name
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  # EKS Addons
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+    }
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["m5.large", "m5n.large"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+
+  tags = module.tags.tags
+}
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  # create_role      = false
+  role_name_prefix = "${module.eks.cluster_name}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = module.tags.tags
+}
+
+output "addons" {
+  value = module.eks.cluster_addons
+}
---
+++ ephemeral-vol-test/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # backend "s3" {
+  #   # Update the remote backend below to support your environment
+  #   bucket         = "clowd-haus-iac-us-east-1"
+  #   key            = "eks-reference-architecture/ephemeral-vol-test/us-east-1/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "clowd-haus-terraform-state"
+  #   encrypt        = true
+  # }
+}
+
+provider "aws" {
+  region = local.region
+
+  # assume_role {
+  #   role_arn     = "<TODO>"
+  #   session_name = local.name
+  # }
+}
+
+################################################################################
+# Common Locals
+################################################################################
+
+locals {
+  name        = "ephemeral-vol-test"
+  region      = "us-east-1"
+  environment = "nonprod"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+################################################################################
+# Common Data
+################################################################################
+
+data "aws_availability_zones" "available" {}
+
+################################################################################
+# Common Modules
+################################################################################
+
+module "tags" {
+  source  = "clowdhaus/tags/aws"
+  version = "~> 1.0"
+
+  application = local.name
+  environment = local.environment
+  repository  = "https://github.com/clowdhaus/eks-reference-architecture"
+}
